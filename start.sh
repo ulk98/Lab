19695b34c7370ae2621f3014c50525674f7615f9
ulk98 <ulrichnoukeu98@gmail.com>
2024-08-14 16:42:03 +0000

    config docker-compose.yml file

--- start.sh
+++ start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 #Init environment
-./init_env.sh
+#./init_env.sh
 
 #Build winxp vuln VM
 #./cible/build.sh
 
 #Build attack container image
-docker build -t lab/attack-container ./attaquant
+#docker build -t lab/attack-container ./attaquant
+
+#create Docker macvlan network
+#docker network create -d macvlan \
+#  --subnet=172.19.0.0/24 \
+#  --gateway=172.19.0.1 \
+#  -o parent=eth0 \
+#  macvlan_net
+
+#Launch all containers
+docker-compose up -d
